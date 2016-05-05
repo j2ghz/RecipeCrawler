86f2c4218a3e7d5bb529bad32f02b631278ff487
J2ghz <j2.00ghz@gmail.com>
2016-05-05 21:46:51 +0200

    add gitignore

---
+++ src/main/java/com/j2ghz/recipecrawler/RecipeCrawler.java
@@ -0,0 +1,27 @@
+package com.j2ghz.recipecrawler;
+
+import cpw.mods.fml.common.Mod;
+import cpw.mods.fml.common.event.FMLInitializationEvent;
+import cpw.mods.fml.common.event.FMLPostInitializationEvent;
+import cpw.mods.fml.common.event.FMLPreInitializationEvent;
+
+@Mod(modid = "RecipeCrawler", name = "RecipeCrawler", version = "1.7.10-1.0")
+public class RecipeCrawler {
+    @Mod.Instance("RecipeCrawler")
+    public static RecipeCrawler instance;
+
+    @Mod.EventHandler
+    public void preInit(FMLPreInitializationEvent event){
+
+    }
+
+    @Mod.EventHandler
+    public void init(FMLInitializationEvent event){
+
+    }
+
+    @Mod.EventHandler
+    public void postInit(FMLPostInitializationEvent event){
+
+    }
+}
